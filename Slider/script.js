51048803f34447673a0c19f9d84bd6ad96d452ab
Ian Chin <skyhawk3102000@yahoo.com>
2021-12-10 22:07:35 -0500

    Slider

---
+++ Slider/script.js
@@ -0,0 +1,37 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+
+
+
+let activeSliderIndex = 0
+
+slideLeft.style.top = `-${(slidesLength -1) * 100}vh`
+
+upButton.addEventListener('click',() => changeSlide('up'))
+downButton.addEventListener('click',() => changeSlide('up'))
+
+
+const changeSlide = (direction) =>{
+    const sliderHeight = sliderContainer.clientHeight
+    console.log(sliderHeight)
+
+    if(direction === 'up'){
+        activeSliderIndex++
+        if(activeSliderIndex > slidesLength -1){
+            activeSliderIndex = 0
+        }
+    } else if (direction === 'down'){
+        activeSliderIndex--
+        if(activeSliderIndex < 0){
+            activeSliderIndex = slidesLength -1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSliderIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSliderIndex * sliderHeight}px)`
+}
